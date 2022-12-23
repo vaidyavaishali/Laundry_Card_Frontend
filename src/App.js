d2654a3900bdb2c919615e4107f192ff925c31da
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2022-12-23 21:31:17 +0530

    modified app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-
+// import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import CancelAlert from './cancelAlertPopup/CancelAlert';
+// import Popup from './OrderSuccessFullyPopup/popup';
 import PastOrderPage from './pastOrderPage/PastOrder';
-
+import PastOrderSummery from './PastOrderSummeryPage/PastOrderSummery';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <PastOrderPage />
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<PastOrderPage />}/>
+        <Route path="/summery" element = {<PastOrderSummery />}/>
+        <Route path="/cancel/alert" element = {<CancelAlert />}/>
+        {/* <Route path="/success" element = {<Popup />}/> */}
+        
+      </Routes>
 
     </div>
   );
 }
-// import PastOrderPage from './pastOrderPage/PastOrder';
 
 export default App;
